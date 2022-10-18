474ef819d1dbe8f9db008b634b3399a83a7958db
FujiHai <liufushihai@gmail.com>
2022-10-18 22:34:40 +0800

    chore: init project

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import { createRequire } from "module";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+const require = createRequire(import.meta.url);
+const pkg = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
