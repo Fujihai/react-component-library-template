cc98497af7db8d0250c3907c4aa9bd27b7615b09
FujiHai <liufushihai@gmail.com>
2022-10-21 07:38:33 +0800

    feat: add css、jest、storybook

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import { postcss } from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const require = createRequire(import.meta.url);
 const pkg = require("./package.json");
@@ -23,14 +26,20 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
+
       nodeResolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+
+      terser(),
     ],
   },
   {
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
